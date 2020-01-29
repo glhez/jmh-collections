5543f3de880a9d2df8586beae25af601cb28cd2d
Gaël Lhez <glhez@users.noreply.github.com>
2020-01-29 23:21:38 +0100

    some fixes

---
+++ benchmark.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+benchmark() {
+  local jar=$1
+  shift
+  for jdk_home; do
+    local java_exec="${jdk_home}/bin/java"
+
+    if [[ -x "${java_exec}" ]]; then
+      echo "running using ${jdk_home}"
+
+      local -A properties=()
+      while read -r property_line; do
+        local key="${property_line%% = *}"
+        local value="${property_line#* = }"
+        properties["$key"]="$value"
+      done < <( "${java_exec}" -XshowSettings:properties -version 2>&1 | grep -Po 'java.(vendor|version) = .+' )
+
+      # not all JDK returns the valid vendor, help them
+      if [[ "${jdk_home,,}" =~ redhat ]]; then
+        properties[java.vendor]=RedHat
+      fi
+
+      for key in "${!properties[@]}"; do
+        echo ":: ${key}: ${properties[$key]}"
+      done
+
+      "${java_exec}" -jar "$jar" -rf json 'com\.github\.glhez\.jmhcollections\.IntegerToString\.emptyStringPlusInt'
+      break
+
+    fi
+  done
+}
+
+benchmark './java-1.8/target/jmh-java-1.8-benchmark.jar' "${JAVA8_HOME}"  "${JAVA8_OPENJ9_HOME}" "${JAVA8_REDHAT_HOME}"
+benchmark './java-11/target/jmh-java-11-benchmark.jar'   "${JAVA11_HOME}" "${JAVA11_OPENJ9_HOME}"
+benchmark './java-13/target/jmh-java-13-benchmark.jar'   "${JAVA13_HOME}" "${JAVA13_OPENJ9_HOME}"
